0e7ea6c6e9315369b3d701e0cbc26f685407b49e
maccyber <jonas.enge@gmail.com>
2017-02-09 00:27:32 +0100

    genesis commit

---
+++ data/index.js
@@ -0,0 +1,7 @@
+const A = require('./en/agreeableness')
+const E = require('./en/extraversion')
+const N = require('./en/neuroticism')
+const C = require('./en/conscientiousness')
+const O = require('./en/openness_to_experience')
+
+module.exports = Object.assign([], [ A, E, N, C, O ])
--- tst.js
+++ tst.js
@@ -1,3 +1,18 @@
-const e = require('./data/extraversion')
+'use strict'
 
-console.log(e.shortDescription)
+const results = require('./data/index')
+
+const testData = [
+  {
+    domain: 'A',
+    score: 3,
+    result: 'neutral'
+  },
+  {
+    domain: 'E',
+    score: 1,
+    result: 'negative'
+  }
+]
+
+console.log(results)
